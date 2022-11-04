078589ae2abefe065f2aaf4b171458e96e75450d
DOODER(Michael Smith) <40189403+dooder-codes@users.noreply.github.com>
2022-11-04 10:05:55 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const discord = require("discord.js");
+const config = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
+require("dotenv").config();
+
+const $PREFIX = config.default_prefix;
+
+const intents = [
+  discord.GatewayIntentBits.Guilds,
+  discord.GatewayIntentBits.GuildMessages,
+  discord.GatewayIntentBits.MessageContent,
+];
+const client = new discord.Client({
+  disableEveryone: false,
+  intents,
+});
+
+client.commands = new discord.Collection();
+
+client.once(discord.Events.ClientReady, (c) => {
+  client.user.setStatus("dnd");
+  console.log(`${c.user.tag} is Online!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith($PREFIX)) return;
+
+  const args = message.content.slice($PREFIX.length);
+
+  // console.log(
+  //   `${client.user.tag} has picked up a message sent to the server: \n"${message.content}"`
+  // );
+  // bot mentioned
+  const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
+  if (message.content.match(prefixMention)) {
+    console.log(`${client.user.tag} was mentioned`);
+    message.reply(`My prefix is \`${config.default_prefix}\``);
+  }
+});
+
+client.login(process.env.TOKEN);
