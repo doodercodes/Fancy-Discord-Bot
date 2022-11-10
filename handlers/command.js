f82b118d200f7276ae37877b6015dce9cd8c4c41
DOODER(Michael Smith) <40189403+dooder-codes@users.noreply.github.com>
2022-11-10 18:43:26 -0500

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -2,18 +2,30 @@ const fs = require("node:fs");
 const path = require("node:path");
 const ascii = require("ascii-table");
 
-let table = new ascii("Commands");
+const commandPath = path.join(__dirname, "../commands");
+let table = new ascii(`${commandPath}`);
 table.setHeading("Command", "Load status");
 
-const commandPath = path.join(__dirname, "../commands");
 // console.log(`${commandPath}`);
 
 module.exports = (client) => {
-  let commands;
-//   fs.readFileSync(commandPath)
-//   fs.readdirSync(commandPath).forEach((dir) => {
-  
-//     // commands = fs.readdirSync(`${commandPath}/${dir}/`);
-//     // console.log(commands);
-//   });
+  fs.readdirSync(commandPath).forEach((dir) => {
+    // if (condition) {}
+    const cmds = `${commandPath}/${dir}/`;
+    let commands = fs.readdirSync(cmds).filter((file) => file.endsWith(".js"));
+    for (const file of commands) {
+      let pull = require(`${cmds}/${file}`);
+      if (pull.name) {
+        client.commands.set(pull.name, pull);
+        table.addRow(file, "✅");
+      } else {
+        table.addRow(
+          file,
+          `❌  -> missing a help.name, or help.name is not a string.`
+        );
+        continue;
+      }
+    }
+  });
+  console.log(table.toString());
 };
