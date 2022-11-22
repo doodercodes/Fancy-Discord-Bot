eeeec34d6821b2948383d49dccf2613a7a4db7a8
DOODER(Michael Smith) <40189403+dooder-codes@users.noreply.github.com>
2022-11-22 16:39:59 -0500

    deleted dirs containing commands

--- commands/ping/ping.js
+++
@@ -1,9 +0,0 @@
-const obj = {
-  name: "Ping",
-  category: `Utility`,
-  description: "Pings the bot",
-  run: async (client, message, args) => {
-    message.reply("PONG!");
-  },
-};
-module.exports = obj
